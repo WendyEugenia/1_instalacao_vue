cfffeebb85a898cdec5bf73dc0173a6b6ac42e1e
WendyEugenia <wendy.t.n@hotmail.com>
2022-09-29 15:34:52 -0300

    atualização

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const MyNameApp = {
     data() {
         return {
-            name: "Wendy",
-            age: "27",
-            student: "Administração"
+            name: "",
+            age: 30,
+            input_name:""
+        }
+    },
+    methods: {
+        submitForm(e) {
+
+            e.preventDeFault();
+
+            console.log(this.input_name);
+
+            this.name = this.input_name;
         }
     }
 }
